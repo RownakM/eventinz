fd8399130d407de0137f0ce43afc54dd924930bd
RownakM <rmazumder1033@gmail.com>
2022-12-23 00:38:58 +0530

    Sign In UI

--- lib/custom_fonts/custom_fonts.dart
+++ lib/custom_fonts/custom_fonts.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+var primaryFont = GoogleFonts.roboto();
+
+var regular = GoogleFonts.roboto();
+var Heading = GoogleFonts.roboto(
+    textStyle: TextStyle(fontWeight: FontWeight.w600, fontSize: 16));
+
+var TitleFont = GoogleFonts.roboto(
+    textStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 36));
+
+var TextFont = GoogleFonts.roboto(
+    textStyle: TextStyle(fontWeight: FontWeight.w300, fontSize: 13));
--- lib/start.dart
+++ lib/start.dart
@@ -1,6 +1,8 @@
 import 'package:colorful_safe_area/colorful_safe_area.dart';
 import 'package:eventinz/Color_Scheme/eventinz_colors.dart';
+import 'package:eventinz/custom_fonts/custom_fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Start extends StatefulWidget {
@@ -11,9 +13,11 @@ class Start extends StatefulWidget {
 }
 
 class _StartState extends State<Start> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: ColorfulSafeArea(
           color: primaryColor,
           child: Stack(
@@ -29,9 +33,80 @@ class _StartState extends State<Start> {
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  Gap(105),
                   Text(
                     "Sign In",
-                    style: GoogleFonts.roboto(),
+                    style: TitleFont,
+                  ),
+                  Gap(40),
+                  Expanded(
+                    child: Container(
+                      // color: Colors.red,
+                      padding: EdgeInsets.symmetric(horizontal: 30),
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            TextFormField(
+                              cursorColor: Colors.black45,
+                              decoration: const InputDecoration(
+                                  hintText: "Email",
+                                  focusedBorder: OutlineInputBorder(
+                                      borderSide:
+                                          BorderSide(color: Colors.black45)),
+                                  border: OutlineInputBorder(),
+                                  focusColor: Colors.black45,
+                                  enabledBorder: const OutlineInputBorder(
+                                      borderSide: const BorderSide(
+                                          color: Colors.black45, width: 1))),
+                            ),
+                            Gap(10),
+                            TextFormField(
+                              cursorColor: Colors.black45,
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                  hintText: "Password",
+                                  focusedBorder: OutlineInputBorder(
+                                      borderSide:
+                                          BorderSide(color: Colors.black45)),
+                                  border: OutlineInputBorder(),
+                                  focusColor: Colors.black45,
+                                  enabledBorder: const OutlineInputBorder(
+                                      borderSide: const BorderSide(
+                                          color: Colors.black45, width: 1))),
+                            ),
+                            Gap(5),
+                            Center(
+                              child: Container(
+                                child: TextButton(
+                                  style: TextButton.styleFrom(
+                                    foregroundColor: primaryColor,
+                                  ),
+                                  child: Text(
+                                    "Forgot Password?",
+                                    style: regular,
+                                  ),
+                                  onPressed: () {},
+                                ),
+                                alignment: Alignment.centerRight,
+                              ),
+                            ),
+                            Gap(90),
+                            ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    minimumSize: const Size.fromHeight(50),
+                                    backgroundColor: primaryColor),
+                                onPressed: () {},
+                                child: Text(
+                                  "Login",
+                                  style: regular.copyWith(fontSize: 16),
+                                ))
+                          ],
+                        ),
+                      ),
+                    ),
                   )
                 ],
               )
